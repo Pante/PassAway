604661d45bb46b17614f048ed27bc8fa4892483a
Jillzyt <jillzyt25@gmail.com>
2017-07-04 11:20:09 +0800

    ViewChanges
    ViewChanges

---
+++ PassAway/Controllers/CustomerController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassAway.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult CustomerIndex()
+        {
+            return View();
+        }
+
+        public ActionResult DisplayProducts()
+        {
+            return View();
+        }
+
+    }
+}
