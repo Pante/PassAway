b21e9269d387096c109362d0694788ec8c1cf377
Pante <matthiasngeozhongsim@yahoo.com>
2017-07-20 00:03:09 +0800

    Fuck microsoft

---
+++ PassAway/Models/Shared/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassAway.Models.Shared {
+    public class Product {
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string URL { get; set; }
+        public DateTime Launched { get; set; }
+
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+
+    }
+}
---
+++ PassAway/Models/Shared/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PassAway.Models.Shared {
+    public class ProductRepository {
+
+        private ProductContext context;
+
+
+        public ProductRepository(ProductContext context) {
+            this.context = context;
+        }
+
+
+        public IEnumerable<Product> Products => context.Products;
+
+    }
+}
